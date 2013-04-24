08971f4c761959172738084cf73c77efd5705235
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2013-04-23 22:11:54 -0500

    More stuff part of last commit

---
+++ Src/PredictionGraphs/PlotterAB/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace PlotterAB
+{
+  public class BoolToVisibilityConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if ((bool?)value == true)
+        return System.Windows.Visibility.Visible;
+      else
+        return System.Windows.Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
